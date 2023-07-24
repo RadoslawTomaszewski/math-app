0ed696e45208c2d451b40cba8aa96b7a97b754d8
radek44413 <radek4441399@gmail.com>
2023-07-24 02:55:37 +0200

    wyciaganie czynnika naprawione

--- src/pages/calculatorsPages/otherCalculators/SquareRootCalculator.tsx
+++ src/pages/calculatorsPages/otherCalculators/SquareRootCalculator.tsx
@@ -7,13 +7,9 @@ import SquareRootNumber from "../../../types/objects/SquareRootCalculator";
 
 const SquareRootCalculator: FC = () => {
   const [inputValue, setInputValue] = useState<string | undefined>("0");
-  const [inputNumber, setInputNumber] = useState<number | null>(0);
-  const [factorizationBefore1, setFactorizationBefore1] = useState<
-    string | null
-  >("0");
-  const [factorizationBefore2, setFactorizationBefore2] = useState<
-    string | null
-  >(null);
+  const [squareRoot, setSquareRoot] = useState<SquareRootNumber | null>(
+    new SquareRootNumber(0)
+  );
   const [errorMessage, setErrorMessage] = useState<string[]>([]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -29,8 +25,7 @@ const SquareRootCalculator: FC = () => {
       inputValue === "0"
     ) {
       errors = [];
-      setInputNumber(inputValueNumber);
-      factorizeBefore1(inputValueNumber);
+      setSquareRoot(new SquareRootNumber(inputValueNumber));
     } else {
       if (!Number.isInteger(inputValueNumber) || inputValueNumber === 0)
         errors.push("Wprowadzona wartość nie jest liczbą całkowitą");
@@ -39,101 +34,54 @@ const SquareRootCalculator: FC = () => {
       if (inputValueNumber >= 10000000)
         errors.push("Maksymalna wartość to 10000000");
       setErrorMessage(errors);
-      setFactorizationBefore1(null);
+      setSquareRoot(null);
     }
   };
 
-  const factorizeBefore1 = (num: number) => {
-    let divident = num;
-    let factors = [];
-    if (divident === 0) {
-      setFactorizationBefore1("0");
-      return;
-    }
-    if (divident === 1) {
-      setFactorizationBefore1("1");
-      return;
-    }
-    let divider = 2;
-    while (divider <= divident) {
-      if (divident % divider === 0) {
-        factors.push(divider);
-        divident = divident / divider;
-        divider = 1;
-      }
-      divider += 1;
-    }
-    factorizeBefore2(factors);
-    setFactorizationBefore1(factors.join(" \\cdot "));
-    factors = [];
-  };
+  const steps = [
+    squareRoot?.getStep1(),
+    squareRoot?.getStep2(),
+    squareRoot?.getStep3(),
+    squareRoot?.getStep4(),
+  ];
 
-  const factorizeBefore2 = (factors: number[]) => {
-    const preSquares: number[] = [];
-    const underSquares: number[] = factors.slice();
-    const indicesToRemove: number[] = [];
-
-    for (let i = 0; i < underSquares.length; i++) {
-      for (let j = i + 1; j < underSquares.length; j++) {
-        if (underSquares[i] === underSquares[j]) {
-          preSquares.push(underSquares[i]);
-          indicesToRemove.push(i, j);
-          break;
-        }
-      }
-    }
-    for (let i = indicesToRemove.length - 1; i >= 0; i--) {
-      underSquares.splice(indicesToRemove[i], 1);
-    }
-
-    const preSquaresString = preSquares.join(" \\cdot ");
-    const underSquaresString = underSquares.join(" \\cdot ");
-
-    if (underSquaresString === "") {
-      setFactorizationBefore2(`${preSquaresString}`);
-    } else {
-      setFactorizationBefore2(
-        `${preSquaresString}\\sqrt{${underSquaresString}}`
-      );
-    }
-  };
-
-  const step1 = new SquareRootNumber(36);
+  const uniqueSteps = Array.from(
+    new Set(steps.filter((step) => step !== undefined))
+  );
 
   return (
-    <div className="flex flex-col justify-center w-full items-center">
-      <Title
-        text="Wyciąganie czynnika całkowitego przed pierwiastek"
-        size="H2"
-      />
-      <p>Wprowadź liczbę naturalną dodatnią:</p>
-      <input
-        className="m-2 text-center w-3/12 font-math bg-bgColor border-2 border-black rounded appearance-none focus:outline-none"
-        placeholder="0"
-        type="number"
-        value={inputValue}
-        onChange={handleInputChange}
-        name="square"
-        min="0"
-      />
-
-      {errorMessage.length !== 0 ? (
-        <div className="text-red-600">
-          {errorMessage.map((error) => (
-            <div>{error}</div>
-          ))}
+    <div className="p-5 justify-center flex">
+      <div className="p-3 border-2 border-navColor rounded w-fit min-h-[370px]">
+        <div className="flex flex-col justify-center w-full items-center">
+          <Title
+            text="Wyciąganie czynnika całkowitego przed pierwiastek"
+            size="H2"
+          />
+          <p className="pt-4">Wprowadź liczbę naturalną pod pierwiastkiem:</p>
+          <input
+            className="m-2 text-center w-[80px] font-math bg-bgColor border-2 border-black rounded appearance-none focus:outline-none"
+            placeholder="0"
+            type="number"
+            value={inputValue}
+            onChange={handleInputChange}
+            name="square"
+            min="0"
+          />
+          <div className="pt-4">Rozwiązanie:</div>
+          {errorMessage.length !== 0 ? (
+            <div className="text-red-600">
+              {errorMessage.map((error) => (
+                <div key={error}>{error}</div>
+              ))}
+            </div>
+          ) : (
+            <>
+              {uniqueSteps.map((step, index) => (
+                <Formula key={index} formula={step ?? ""} />
+              ))}
+            </>
+          )}
         </div>
-      ) : (
-        <>
-          <Formula formula={`\\sqrt{${inputNumber}}=`} />
-          <Formula formula={`=\\sqrt{${factorizationBefore1}}`} />
-          <Formula formula={`=${factorizationBefore2}`} />
-        </>
-      )}
-      <div>_______________</div>
-      <div>
-        <Formula formula={step1.getStep1()} />
-        <Formula formula={step1.getStep2()} />
       </div>
     </div>
   );
--- src/types/objects/SquareRootCalculator.ts
+++ src/types/objects/SquareRootCalculator.ts
@@ -1,36 +1,122 @@
 class SquareRootNumber {
-    constructor(private num: number) {}
-    
-    getStep1(): string{
-        return `\\sqrt{${this.num}}`
+  private factors: number[] = [];
+  private preFactors: number[] = [];
+  private underFactors: number[] = [];
+  private preSquare: number | null = null;
+  private underSquare: number | null = null;
+
+
+    constructor(private num: number) {
+      this.setFactors();
+      this.setpreFactorsAndUnderFactors();
+      this.setPreSquareAndUnderSquare();
     }
 
-    getStep2():string{
-        let divident = this.num;
-        const factors = [];
-        if (divident === 0) {
-          return `0`;
-        }
-        if (divident === 1) {
-            return `1`;
-        }
-        let divider = 2;
+
+    private setFactors():void{
+      let divident = this.num;
+      if(divident === 0 || divident === 1) this.factors.push(divident);
+      let divider = 2;
         while (divider <= divident) {
           if (divident % divider === 0) {
-            factors.push(divider);
+            this.factors.push(divider);
             divident = divident / divider;
             divider = 1;
           }
           divider += 1;
         }
-        const step2 = factors.join(" \\cdot ");
-        return `\\sqrt{${step2}}`
     }
 
+    private setpreFactorsAndUnderFactors(): void {
+      this.underFactors = this.factors.slice();
+      const indicesToRemove: number[] = [];
+    
+      for (let i = 0; i < this.underFactors.length; i++) {
+        let foundDuplicate = false;
+        for (let j = i + 1; j < this.underFactors.length; j++) {
+          if (this.underFactors[i] === this.underFactors[j]) {
+            this.preFactors.push(this.underFactors[i]);
+            indicesToRemove.push(i, j);
+            foundDuplicate = true;
+            break;
+          }
+        }
+        if (foundDuplicate) {
+          break;
+        }
+      }
+      while (indicesToRemove.length > 0) {
+        for (let i = indicesToRemove.length - 1; i >= 0; i--) {
+          this.underFactors.splice(indicesToRemove[i], 1);
+        }
+        indicesToRemove.length = 0;
+        for (let i = 0; i < this.underFactors.length; i++) {
+          let foundDuplicate = false;
+          for (let j = i + 1; j < this.underFactors.length; j++) {
+            if (this.underFactors[i] === this.underFactors[j]) {
+              this.preFactors.push(this.underFactors[i]);
+              indicesToRemove.push(i, j);
+              foundDuplicate = true;
+              break;
+            }
+          }
+          if (foundDuplicate) {
+            break;
+          }
+        }
+      }
+    }
+    
+    private setPreSquareAndUnderSquare():void{
+      let productPre = 1;
+      let productUnder = 1;
 
+      this.preFactors.forEach((element) => {
+        productPre *= element;
+      });
+      this.underFactors.forEach((element) => {
+        productUnder *= element;
+      });
 
+      this.preSquare = productPre;
+      this.underSquare = productUnder;
+    }
+   
+    getFactors(): string{
+      return this.factors.join(", ")
+    }
+    
+    getStep1(): string{
+      return `\\sqrt{${this.num}}`
+    }
 
+    getStep2():string{
+      const step2 = this.factors.join(" \\cdot ");
+      return `\\sqrt{${step2}}`
+    }
+    getStep3():string{
+      const preFactorsString = this.preFactors.join(" \\cdot ");
+      const underFactorsString = this.underFactors.join(" \\cdot ");
+  
+      if (underFactorsString === "") {
+        return `${preFactorsString}`;
+      } else {
+        return `${preFactorsString}\\sqrt{${underFactorsString}}`;
+      }
+    }
+    getStep4():string{
+      if (this.underSquare === 1) return `${this.preSquare}`
+      if (this.preSquare === 1) return `\\sqrt{${this.underSquare}}`;
+      return `${this.preSquare}\\sqrt{${this.underSquare}}`;
+    }
+    getPreSquare():number | null {
+      return this.preSquare;
+    }
+    getUnderSquare():number | null {
+      return this.underSquare;
+    }
+    }
 
-  }
+  
     
   export default SquareRootNumber;
