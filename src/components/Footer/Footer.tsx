eb3365b8e627db50dfc1e8d06e8b0d5343e13c86
radek44413 <radek4441399@gmail.com>
2024-03-11 02:42:39 +0100

    flex correction commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-col bg-neutral-200 min-h-[54px] text-center text-neutral-700 flex items-center justify-center">
+      <div className="flex flex-col bg-neutral-200 min-h-[54px] text-center text-neutral-700 items-center justify-center">
         <span>© 2023 - 2024 Radosław Tomaszewski </span>
         <span> <b>rtomaszewski@majza.eu</b></span>
       </div>
